e1a111f0e5eb9b84210bf5e17703d8af522fcf82
Mihai Ungureanu <mihai.ungureanu@softwareag.com>
2022-10-03 21:57:25 +0200

    Reached a usable full environment

--- run-configurations/api-mockup-test-01/scripts/jmeter/runTests.sh
+++ run-configurations/api-mockup-test-01/scripts/jmeter/runTests.sh
@@ -10,11 +10,17 @@ done
 declare -i usersNo=${MIN_USERS_NO:-2}
 declare -i usersInc=${USER_NO_INCREMENT:-2}
 declare -i maxUsersNo=${MAX_USERS_NO:-8}
+
 declare -i loopCount=${LOOP_COUNT:-20}
 declare -i slowMultiplier=${SLOW_MULTIPLIER:-200}
 declare -i longLoopCount=${loopCount}*200
+
 declare -i fastMillis=${FAST_MILLIS:-10}
 declare -i slowMillis=${fastMillis}*${slowMultiplier}
+
+declare -i minBigBodyBytes=${MIN_BIG_BODY_BYTES:-10000}
+declare -i bigBodyBytesIncrement=${BIG_BODY_BYTES_INCREMENT:-25000}
+declare -i maxBigBodyBytes=${MAX_BIG_BODY_BYTES:-300000}
 
 echo "Test parameters:"
 echo "loopCount=${loopCount}"
@@ -27,20 +33,26 @@ echo "slowMillis=${slowMillis}"
 while [ ${usersNo} -le ${maxUsersNo} ]; do
   echo "Running tests with ${usersNo} users"
 
-  jmeter -n \
-    -JserviceHostname=msr-test-01 \
-    -JservicePort=5555 \
-    -JserviceAdminPassword=${IS_ADMIN_PWD} \
-    -JnoOfUsers=${usersNo} \
-    -JloopCount=${loopCount} \
-    -JquickThreadGroup.loopCount=${longLoopCount} \
-    -JfastServiceSleepTime=${fastMillis} \
-    -JslowServiceSleepTime=${slowMillis} \
-    -JfastServiceBodyChars=48 \
-    -JslowServiceBodyChars=500000 \
-    -t /tmp/suite.jmx
+  declare -i crtBigBodySize=${minBigBodyBytes}
+  while [ ${crtBigBodySize} -le ${maxBigBodyBytes} ]; do
+    echo "Running tests with ${usersNo} users with ${crtBigBodySize} big service body size"
+    jmeter -n \
+      -JserviceHostname=msr-test-01 \
+      -JservicePort=5555 \
+      -JserviceAdminPassword=${IS_ADMIN_PWD} \
+      -JnoOfUsers=${usersNo} \
+      -JloopCount=${loopCount} \
+      -JquickThreadGroup.loopCount=${longLoopCount} \
+      -JfastServiceSleepTime=${fastMillis} \
+      -JslowServiceSleepTime=${slowMillis} \
+      -JfastServiceBodyChars=48 \
+      -JslowServiceBodyChars=${crtBigBodySize} \
+      -Jprometheus.ip=0.0.0.0 \
+      -t /tmp/suite.jmx
 
-  sleep 20
+    sleep 30
+    declare -i crtBigBodySize+=${bigBodyBytesIncrement}
+  done 
   declare -i usersNo+=${usersInc}
 done
 
